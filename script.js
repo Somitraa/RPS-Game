44d83fdfb6ce681428a37634886f5c805243dd5c
Somitraa <guptasomitra@gmail.com>
2025-02-07 13:40:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function rock(){
+    let value = Math.floor(Math.random()*3);
+    let s =0;
+
+    if (value == s) {
+        alert("tie");
+    }
+    else if (value == 1) {
+        alert("Computer WON");
+    }
+    else{
+        alert("You Won")
+    }
+}
+function paper(){
+    let value = Math.floor(Math.random()*3);
+    let s =1;
+
+    if (value == s) {
+        alert("tie");
+    }
+    else if (value == 2) {
+        alert("Computer WON");
+    }
+    else{
+        alert("You Won")
+    }
+}
+function scissor(){
+    let value = Math.floor(Math.random()*3);
+    let s =2;
+
+    if (value == s) {
+        alert("tie");
+    }
+    else if (value == 2) {
+        alert("Computer WON");
+    }
+    else{
+        alert("You Won")
+    }
+}
